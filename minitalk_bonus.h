33cfbdcb879f5d51c6358d13e2b3cbfc5f46803a
ylamsiah <yousseflamssieh@gmail.com>
2023-03-19 02:51:49 +0000

    Zoro - (- _ *)

--- minitalk_bonus.h
+++ minitalk_bonus.h
@@ -6,14 +6,14 @@
 /*   By: ylamsiah <ylamsiah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/17 13:39:43 by ylamsiah          #+#    #+#             */
-/*   Updated: 2023/03/19 02:39:02 by ylamsiah         ###   ########.fr       */
+/*   Updated: 2023/03/19 02:43:50 by ylamsiah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_BONUS_H
 # define MINITALK_BONUS_H
 
-# include  <unistd.h>
+# include <unistd.h> 
 # include <stdlib.h>
 # include <stdio.h>
 # include <signal.h>
